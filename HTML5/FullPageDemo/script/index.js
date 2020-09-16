a84cccf451ebb7a82f93fa12f1d7836b52dd28ff
Gravity <1987391831@qq.com>
2020-09-16 21:46:40 +0800

    自动轮播图

--- HTML5/FullPageDemo/script/index.js
+++ HTML5/FullPageDemo/script/index.js
@@ -1,16 +1,18 @@
 const btn_Click = document.getElementsByClassName('btn-click')[0].getElementsByTagName('li');
+const ul_carousel = document.getElementsByClassName('carousel')[0];
+const carousel = ul_carousel.getElementsByTagName('li');
 let index = 0;
-const carousel = document.getElementsByClassName('carousel')[0].getElementsByTagName('li');
+let timer3D = 0;
+let autoIndex = 0;
 
 home3D();
-auto3D();
 
 // 手动轮播
 function home3D() {
   btn_Click[0].classList.add("active");
   for (let i = 0; i < btn_Click.length; i++) {
     btn_Click[i].onclick = function () {
-
+      clearInterval(timer3D);
       if (index != i) {
         btn_Click[index].classList.remove("active");
         carousel[index].classList.remove("active");
@@ -38,12 +40,69 @@ function home3D() {
           carousel[index].classList.add("rightHide");
         }
         index = i;
+        autoIndex = index;
       }
     }
   }
+  auto3D();
 }
 
 function auto3D() {
+  timer3D = setInterval(function () {
+    autoIndex++;
+    if (autoIndex == carousel.length) {
+      autoIndex = 0;
+    }
 
+    btn_Click[index].classList.remove("active");
+    btn_Click[autoIndex].classList.add("active");
+    carousel[autoIndex].classList.remove("leftHide");
+    carousel[autoIndex].classList.remove("leftShow");
+    carousel[autoIndex].classList.remove("rightHide");
+    carousel[autoIndex].classList.add("rightShow");
+
+    carousel[index].classList.remove("leftShow");
+    carousel[index].classList.remove("rightShow");
+    carousel[index].classList.remove("rightHide");
+    carousel[index].classList.add("leftHide");
+
+    index = autoIndex;
+  }, 3000)
+  ul_carousel.onmouseenter = function () {
+    clearInterval(timer3D);
+  }
+
+  ul_carousel.onmouseleave = function () {
+    auto3D();
+  }
 }
 
+const aboutULs = document.getElementsByClassName("pic-line")[0].getElementsByTagName("ul");
+console.log(aboutULs);
+picSegment();
+
+function picSegment() {
+  for (let i = 0; i < aboutULs.length; i++) {
+    change(aboutULs[i]);
+  }
+}
+
+function change(UL) {
+  let src = UL.dataset.src;
+  let w = UL.offsetWidth / 2;
+  let h = UL.offsetHeight / 2;
+  for (let i = 0; i < 4; i++) {
+    let LiNode = document.createElement('li');
+    LiNode.style.width = w + "px";
+    LiNode.style.height = h + "px";
+    let imgNode = document.createElement('img');
+    imgNode.style.left = -(i % 2) * w + "px";
+    imgNode.style.top = -Math.floor(i / 2) * h + "px";
+    imgNode.src = src;
+    LiNode.appendChild(imgNode);
+    UL.appendChild(LiNode);
+  }
+
+  const imgNodes = document.getElementsByClassName
+
+}
